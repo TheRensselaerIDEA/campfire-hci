476d1c1ed33ac7e77f925cfd688450d833f65182
DESKTOP-7OOGRRA\dkfan <dkfang7@gmail.com>
2018-02-20 15:15:08 -0500

    Modularized mouse controller script, now all code can be found within a mouseController object. todo: cases for mouse movement.

--- campfireMouseUtil/index.js
+++ campfireMouseUtil/index.js
@@ -8,21 +8,39 @@ var mouse = require('win-mouse')();
 
 module.exports = function(electronScreen) {
 	//initialize screen variables by copying from source electron app
-	var screen = electronScreen;
-	//TODO: process screen dimensions
-
-	function init()
-	{
-		mouse.on('move', function(xPos, yPos) {
-
+	var mouseController = {
+		setScreens: function(screen)
+		{
+			this.wallScreen = null;
+			this.floorScreen = null;
+			var allScreens = screen.getAllDisplays();
+		  // Wider screen should be the "Wall"
+		  if (allScreens[0].size.width > allScreens[1].size.width)
+		  {
+		  	this.wallScreen = allScreens[0];
+		    this.floorScreen = allScreens[1];
+		  } 
+		  else 
+		  {
+		    this.floorScreen = allScreens[0];
+		    this.wallScreen = allScreens[1];
+		  }
+		},
+		init: function(screen)
+		{
+			this.setScreens(screen);
+			console.log(this.wallScreen);
+			console.log(this.floorScreen);
+			
 			//There is a mapping from f(f_x,f_y) -> w_x,w_y
 			//where f(f_x,f_y) is a function applied to the coordinates of the floor screen
 			//and the output is a corresponding point on the wall screen.
-			console.log(xPos, yPos);
-	});
+			mouse.on('move', function(xPos, yPos) {
+					//console.log(xPos, yPos);
+				}
+			);
+		}
 	}
-
-	init();
-
-
+	//TODO: process screen dimensions
+	mouseController.init(electronScreen);
 }
