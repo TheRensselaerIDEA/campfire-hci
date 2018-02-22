4315e0d12f5c7464c2d631b5993779c59b075ed8
DESKTOP-7OOGRRA\dkfan <dkfang7@gmail.com>
2018-02-22 14:47:22 -0500

    Cleaned up code, added finer check for direction, debugging print statements, added function onFloor to determine if the mouse is within bounds of the floor.

--- campfireMouseUtil/index.js
+++ campfireMouseUtil/index.js
@@ -1,7 +1,6 @@
 // Author 'Tommy Fang'
 // RPI Campfire HCI Project
 'use strict';
-
 const electron = require('electron');
 const robot = require('robotjs');
 var mouse = require('win-mouse')();
@@ -11,7 +10,9 @@ var mouse = require('win-mouse')();
 module.exports = function(electronScreen) {
 
 	//MouseController object stores all code related to mouse utilities.
-var mouseController = {
+
+var mouseController = 
+{
     //Initialize screen variables with electron.
     setScreens: function(screen)
     {
@@ -19,7 +20,6 @@ var mouseController = {
       this.floorScreen = null;
       var allScreens = screen.getAllDisplays();
       var main = screen.getPrimaryDisplay();
-      console.log("PRIMARY");
       // TV["id"] = 2779098405
       //main monitor id = 2528732444
       if (allScreens[0].size.width > allScreens[1].size.width)
@@ -34,35 +34,103 @@ var mouseController = {
       }
     },
 
+    rotateMouse: function()
+    {
+      this.wallSize = this.wallScreen.size;
+      var w = this.wallSize.width/2;
+      var h = (this.wallSize.height/2);
+      var twoPI = Math.PI * 2;
+      var x = 0, y = 0;
+      var radius = 1000;
 
+      //Moves mouse in a circle
+      for (var a = 0; a < twoPI; a+=0.1)
+      {
+        y = h + radius * Math.sin(a);
+        x = w + radius * Math.cos(a);
+        robot.moveMouse(x, y);
+        console.log("ROBOT MOVING:" + x + "," + y);
+      }
+    },
+    listen: function()
+    {
+        var wall = this.wallScreen,
+            floor = this.floorScreen;
+
+        var mPos = robot.getMousePos();
+        var lastX = mPos.x, lastY = mPos.y;
+        //determines if the mouse cursor is within the boundaries of the floor, else we are on the wall.
+        //These functions are created in local scope to be used by the event listener.
+        var onFloor = function(x,y, floor)
+        {
+          var fb = floor.bounds;
+
+          if ((x <= fb.x + fb.width && x >= fb.x) && 
+             (y <= fb.y + fb.height && y >= fb.y))
+          {
+            return true;
+          }
+          return false;
+        }
+        //Checks change between last position(lx, ly) current position(x,y)
+        //
+        var getDir = function(x, y, lx, ly)
+        {
+          var hDir, vDir;
+
+          hDir = lx < x ? "right" : "left";
+          vDir = ly < y ? "down" : "up";
+
+          vDir = ly - y == 0 ?  vDir = "null" : vDir;
+          hDir = lx - x == 0 ?  hDir = "null" : hDir;
+          return [hDir, vDir];
+        }
+
+        
+        //There is a mapping from f(f_x,f_y) -> w_x,w_y
+        //where f(f_x,f_y) is a function applied to the coordinates of the floor screen
+        //and the output is a corresponding point on the wall  screen.
+
+        mouse.on('move', function(xPos, yPos) 
+        {
+          var dir = getDir(xPos, yPos, lastX, lastY)
+          lastX = xPos;
+          lastY = yPos;
+
+          var isOnFloor = onFloor(xPos, yPos, floor);
+
+          //DEBUGGING!!!
+          debug = "Mouse movement\n";
+          for (var i = 0; i < dir.length; i++)
+          {
+            if (dir[i] != "null")
+            {
+              debug += dir[i] + " "
+            }
+          }
+          debug += "\nOnFloor = " + isOnFloor
+          debug += "\n(LastX, LastY): " + "(" + lastX + "," + lastY + ")";
+          debug += "\n(Mx, My): " + "(" + xPos + "," + yPos + ")";
+          console.log(debug);
+
+/*          if (xPos <= 1 && hDir == "left")
+          {
+            console.log("Condition met");
+            robot.moveMouse(wall.bounds.x, wall.bounds.y);
+          }
+*/
+        }
+        );
+    },
     init: function(screen)
     {
-      this.setScreens(screen);
-    /*  console.log(this.wallScreen);
-      console.log(this.floorScreen);
-    */  //There is a mapping from f(f_x,f_y) -> w_x,w_y
-      //where f(f_x,f_y) is a function applied to the coordinates of the floor screen
-      //and the output is a corresponding point on the wall screen.
-
-      var lastX = 0, lastY = 0;
-      mouse.on('move', function(xPos, yPos) 
-      {
-        //Get direction of mouse
-        var hDir, vDir = "NULL";
-        //check dx (change in x) and dy (change in y) positions.
-        hDir = lastX < xPos ? "right" : "left";
-        vDir = lastY < yPos ? "down" : "up";
-
-        vDir = lastY - yPos == 0 ?  vDir = "null" : vDir;
-        hDir = lastX - xPos == 0 ?  hDir = "null" : hDir;
-
-        console.log(lastX +','+lastY)
-        lastX = xPos;
-        lastY = yPos;
-      }
-      );
+      this.screens = this.setScreens(screen);
+      this.listen();
+     // this.rotateMouse();
     }
-  }
+}
 	//TODO: process screen dimensions
-	mouseController.init(electronScreen);
+mouseController.init(electronScreen);
+	
+	
 }
--- exampleproject/main.js
+++ exampleproject/main.js
@@ -17,7 +17,8 @@ var floorWindow = null;
 var mainWindow = null;
 
 
-var mouseController = {
+var mouseController = 
+{
     //Initialize screen variables with electron.
     setScreens: function(screen)
     {
@@ -25,8 +26,6 @@ var mouseController = {
       this.floorScreen = null;
       var allScreens = screen.getAllDisplays();
       var main = screen.getPrimaryDisplay();
-      //console.log(allScreens);
-     // console.log(main);
       // TV["id"] = 2779098405
       //main monitor id = 2528732444
       if (allScreens[0].size.width > allScreens[1].size.width)
@@ -36,26 +35,19 @@ var mouseController = {
       } 
       else 
       {
-          this.floorScreen = allScreens[0];
-          this.wallScreen = allScreens[1];
-        }
+        this.floorScreen = allScreens[0];
+        this.wallScreen = allScreens[1];
+      }
     },
 
-
-    init: function(screen)
+    rotateMouse: function()
     {
-      this.setScreens(screen);
-    
-
-      //There is a mapping from f(f_x,f_y) -> w_x,w_y
-      //where f(f_x,f_y) is a function applied to the coordinates of the floor screen
-      //and the output is a corresponding point on the wall screen.
-      wallSize = this.wallScreen.size;
-      w = wallSize.width/2;
-      h = wallSize.height/2;
-      twoPI = Math.PI * 2;
+      this.wallSize = this.wallScreen.size;
+      var w = this.wallSize.width/2;
+      var h = (this.wallSize.height/2);
+      var twoPI = Math.PI * 2;
       var x = 0, y = 0;
-      radius = 500;
+      var radius = 1000;
 
       //Moves mouse in a circle
       for (var a = 0; a < twoPI; a+=0.1)
@@ -65,27 +57,83 @@ var mouseController = {
         robot.moveMouse(x, y);
         console.log("ROBOT MOVING:" + x + "," + y);
       }
+    },
+    listen: function()
+    {
+        var wall = this.wallScreen,
+            floor = this.floorScreen;
 
-      //Get direction of mouse
-      var lastX = 0, lastY = 0;
-      mouse.on('move', function(xPos, yPos) 
-      {
-        var hDir, vDir = "NULL";
-        //check dx (change in x) and dy (change in y) positions.
-        hDir = lastX < xPos ? "right" : "left";
-        vDir = lastY < yPos ? "down" : "up";
+        var mPos = robot.getMousePos();
+        var lastX = mPos.x, lastY = mPos.y;
+        //determines if the mouse cursor is within the boundaries of the floor, else we are on the wall.
+        //These functions are created in local scope to be used by the event listener.
+        var onFloor = function(x,y, floor)
+        {
+          var fb = floor.bounds;
 
-        vDir = lastY - yPos == 0 ?  vDir = "null" : vDir;
-        hDir = lastX - xPos == 0 ?  hDir = "null" : hDir;
-		
-	       //	console.log(vDir + "," + hDir + "(" + lastX + "," + lastY + ")" + "(" + xPos + "," + yPos + ")");
-        //console.log(lastX +','+lastY)
-        lastX = xPos;
-        lastY = yPos;
-      }
-      );
+          if ((x <= fb.x + fb.width && x >= fb.x) && 
+             (y <= fb.y + fb.height && y >= fb.y))
+          {
+            return true;
+          }
+          return false;
+        }
+        //Checks change between last position(lx, ly) current position(x,y)
+        //
+        var getDir = function(x, y, lx, ly)
+        {
+          var hDir, vDir;
+
+          hDir = lx < x ? "right" : "left";
+          vDir = ly < y ? "down" : "up";
+
+          vDir = ly - y == 0 ?  vDir = "null" : vDir;
+          hDir = lx - x == 0 ?  hDir = "null" : hDir;
+          return [hDir, vDir];
+        }
+
+        
+        //There is a mapping from f(f_x,f_y) -> w_x,w_y
+        //where f(f_x,f_y) is a function applied to the coordinates of the floor screen
+        //and the output is a corresponding point on the wall  screen.
+
+        mouse.on('move', function(xPos, yPos) 
+        {
+          var dir = getDir(xPos, yPos, lastX, lastY)
+          lastX = xPos;
+          lastY = yPos;
+
+          var isOnFloor = onFloor(xPos, yPos, floor);
+
+          //DEBUGGING!!!
+          debug = "Mouse movement\n";
+          for (var i = 0; i < dir.length; i++)
+          {
+            if (dir[i] != "null")
+            {
+              debug += dir[i] + " "
+            }
+          }
+          debug += "\nOnFloor = " + isOnFloor
+          debug += "\n(LastX, LastY): " + "(" + lastX + "," + lastY + ")";
+          debug += "\n(Mx, My): " + "(" + xPos + "," + yPos + ")";
+          console.log(debug);
+/*          if (xPos <= 1 && hDir == "left")
+          {
+            console.log("Condition met");
+            robot.moveMouse(wall.bounds.x, wall.bounds.y);
+          }
+*/
+        }
+        );
+    },
+    init: function(screen)
+    {
+      this.screens = this.setScreens(screen);
+      this.listen();
+     // this.rotateMouse();
     }
-  }
+}
 function createWindow () {
 
   var screenElectron = electron.screen;
